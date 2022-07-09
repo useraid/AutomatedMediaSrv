d6d43353e9bb2620d8a759c0ae105b5384a31dda
useraid <useraid@pm.me>
2022-07-09 20:49:47 +0530

    added auto folder maker

---
+++ folder.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /
+mkdir data dockdata
+cd data
+mkdir tvseries movies
+cd ..
+cd dockdata
+mkdir sonarr radarr qbittorrent jackett jellyfin bazarr
+cd qbittorrent
+mkdir appdata downloads
+cd appdata 
+mkdir config
