3d38259e44dfbb5b4e7b1a6cbddaaadd71042d33
useraid <useraid@pm.me>
2022-08-16 14:37:31 +0530

    improved status script

--- status.sh
+++ status.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
-
 url="webhook_url"
-
 websites_list="http://IP_ADDR:PORT"
-
+curl -H "Content-Type: application/json" -X POST -d '{"content":"'" $(date) \n***Services*** "'"}'  $url
 for website in ${websites_list} ; do
         status_code=$(curl --write-out %{http_code} --silent --output /dev/null -L ${website})
 
         if [[ "$status_code" -ne 200 ]] ; then
-            curl -H "Content-Type: application/json" -X POST -d '{"content":"'"${website} : ${status_code}"'"}'  $url
+            if [[ "$website" = http://*.*.*.*:8096 ]] ; then
+                domain="Jellyfin"
+            elif [[ "$website" = http://*.*.*.*:9000 ]] ; then
+                domain="Portainer"
+            elif [[ "$website" = http://*.*.*.*:8989 ]] ; then
+                domain="Sonarr"
+            elif [[ "$website" = http://*.*.*.*:7878 ]] ; then
+                domain="Radarr"
+            elif [[ "$website" = http://*.*.*.*:8090 ]] ; then
+                domain="qBittorrent"
+            elif [[ "$website" = http://*.*.*.*:5055 ]] ; then
+                domain="Jellyseerr"
+            fi
+            # POST request to Discord Webhook with the domain name and the HTTP status code
+            curl -H "Content-Type: application/json" -X POST -d '{"content":"'"${website} is down with SC : ${status_code}"'"}'  $url
         else
-            curl -H "Content-Type: application/json" -X POST -d '{"content":"'"${website} : ${status_code}"'"}'  $url
+            if [[ "$website" = http://*.*.*.*:8096 ]] ; then
+                domain="Jellyfin"
+            elif [[ "$website" = http://*.*.*.*:9000 ]] ; then
+                domain="Portainer"
+            elif [[ "$website" = http://*.*.*.*:8989 ]] ; then
+                domain="Sonarr"
+            elif [[ "$website" = http://*.*.*.*:7878 ]] ; then
+                domain="Radarr"
+            elif [[ "$website" = http://*.*.*.*:8090 ]] ; then
+                domain="qBittorrent"
+            elif [[ "$website" = http://*.*.*.*:5055 ]] ; then
+                domain="Jellyseerr"
+            fi
+            curl -H "Content-Type: application/json" -X POST -d '{"content":"'"${domain} is up and running with SC : ${status_code}"'"}'  $url
         fi
 done
