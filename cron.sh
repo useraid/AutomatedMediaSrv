536e41d7cb8bd9e7af0a9858689466f7e9e51b39
useraid <useraid@pm.me>
2022-09-21 16:23:11 +0530

    notification frequency selection added

--- cron.sh
+++ cron.sh
@@ -4,9 +4,11 @@ echo "Enter the Webhook URL : "
 read webhook
 echo http://$ip > ip.txt
 echo $webhook > webhook.txt
+echo "How often do you want the notifications ? (in Minutes ) : "
+read ti
 
 mkdir /etc/scripts
 cp status.sh /etc/scripts
 cp webhook.txt /etc/scripts
 cp ip.txt /etc/scripts
-(crontab -l 2>/dev/null; echo "*/30 * * * * /etc/scripts/status.sh >/dev/null 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "*/$ti * * * * /etc/scripts/status.sh >/dev/null 2>&1") | crontab -
